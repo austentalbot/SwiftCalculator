15f774ac6ab33c2e238ba495c6a44116b8a999df
Austen Talbot <austentalbot@gmail.com>
2015-02-07 16:22:59 -0800

    Set up basic calculator. Cover first lecture's functionality.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var isTyping = false;
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isTyping == true {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
