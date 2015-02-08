09a889fd0acac667519c23ed893efaa30967b192
Austen Talbot <austentalbot@gmail.com>
2015-02-08 11:17:46 -0800

    Added functionality for operation buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,6 +24,35 @@ class ViewController: UIViewController {
             isTyping = true
         }
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isTyping {
+            enter()
+        }
+        switch operation {
+            case "+": performDuoOperation({ $1 + $0 })
+            case "−": performDuoOperation({ $1 - $0 })
+            case "×": performDuoOperation({ $1 * $0 })
+            case "÷": performDuoOperation({ $1 / $0 })
+            case "√": performUniOperation({ sqrt($0) })
+            default: break
+        }
+    }
+    
+    func performDuoOperation(operation: (Double, Double) -> Double) {
+        if numberStack.count >= 2 {
+            displayValue = operation(numberStack.removeLast(), numberStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performUniOperation(operation: Double -> Double) {
+        if numberStack.count >= 1 {
+            displayValue = operation(numberStack.removeLast())
+            enter()
+        }
+    }
 
     @IBAction func enter() {
         isTyping = false
