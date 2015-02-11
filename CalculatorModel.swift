7fcd57aff961ba179fc56df48bd77b5681860980
Austen Talbot <austentalbot@gmail.com>
2015-02-11 08:38:03 -0800

    Continued on Calculator Model - implemented evaluate function

--- CalculatorModel.swift
+++ CalculatorModel.swift
@@ -10,15 +10,15 @@ import Foundation
 
 class CalculatorModel {
     
-    enum Op {
+    private enum Op {
         case Operand(Double)
         case UniOperation(String, Double -> Double)
         case DuoOperation(String, (Double, Double) -> Double)
     }
     
-    var knownOps = [String: Op]()
+    private var knownOps = [String: Op]()
     
-    var opStack = [Op]()
+    private var opStack = [Op]()
     
     init() {
         knownOps["+"] = Op.DuoOperation("+", +)
@@ -26,6 +26,37 @@ class CalculatorModel {
         knownOps["×"] = Op.DuoOperation("×", *)
         knownOps["÷"] = Op.DuoOperation("÷", { $1 / $0 })
         knownOps["√"] = Op.UniOperation("√", sqrt)
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+                case .Operand(let operand):
+                    return(operand, remainingOps)
+                case .UniOperation(_, let operation):
+                    let operandEvaluation = evaluate(remainingOps)
+                    if let operand = operandEvaluation.result {
+                        return (operation(operand), operandEvaluation.remainingOps)
+                    }
+                case .DuoOperation(_, let operation):
+                    let op1Evaluation = evaluate(remainingOps)
+                    if let operand1 = op1Evaluation.result {
+                        let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                        if let operand2 = op2Evaluation.result {
+                            return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                        }
+                    }
+            }
+            
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        return result
     }
     
     func pushOperand(operand: Double) {
